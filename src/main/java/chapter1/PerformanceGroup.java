5e5ac85c2bebd9732ecdd55d1c7c0eab25f6c72f
FengYannan <fengyannan@mintechai.com>
2019-09-20 16:00:35 +0800

    some practice

--- src/main/java/chapter1/PerformanceGroup.java
+++ src/main/java/chapter1/PerformanceGroup.java
@@ -11,41 +11,24 @@ import java.util.Map;
  * @author fynwin@gmail.com
  */
 public class PerformanceGroup {
+    private final Map<String, Play> plays;
 
-    public String statement(Invoice invoice, Map<String, Play> plays) {
+    public PerformanceGroup() {
+        plays = Maps.newConcurrentMap();
+        plays.put("hamlet", new Play("Hamlet", "tragedy"));
+        plays.put("as-like", new Play("As You Like It", "comedy"));
+        plays.put("othello", new Play("Othello", "tragedy"));
+    }
+
+    public String statement(Invoice invoice) {
         double totalAmount = 0;
         int volumeCredits = 0;
         String result = "Statement for " + invoice.consumer + '\n';
         NumberFormat fmt = NumberFormat.getCurrencyInstance(new Locale("en", "US"));
         for (Performance perf : invoice.performances) {
-            Play play = plays.get(perf.playID);
-            double thisAmount;
-            switch (play.type) {
-                case "tragedy":
-                    thisAmount = 40000;
-                    if (perf.audience > 30) {
-                        thisAmount += 1000 * (perf.audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    thisAmount = 30000;
-                    if (perf.audience > 20) {
-                        thisAmount += 10000 + 500 * (perf.audience - 20);
-                    }
-                    thisAmount += 300 * perf.audience;
-                    break;
-                default:
-                    throw new IllegalStateException("Unkonwn type:" + play.type);
-            }
-            //add volume credits
-            volumeCredits += Math.max(perf.audience - 30, 0);
-            // add extra credit for every then comedy attendees
-            if ("comedy".equals(play.type)) {
-                volumeCredits += Math.floor(perf.audience / 5);
-            }
-
-            // print line for this order
-            result += ' ' + play.name + ": " + fmt.format(thisAmount / 100) + " (" + perf.audience + " seats)\n";
+            double thisAmount = amountFor(perf);
+            volumeCredits += volumeCreditsFor(perf);
+            result += ' ' + playFor(perf).name + ": " + fmt.format(thisAmount / 100) + " (" + perf.audience + " seats)\n";
             totalAmount += thisAmount;
         }
 
@@ -54,18 +37,47 @@ public class PerformanceGroup {
         return result;
     }
 
-    public static void main(String[] args) {
-        Map<String, Play> plays = Maps.newConcurrentMap();
-        plays.put("hamlet", new Play("Hamlet", "tragedy"));
-        plays.put("as-like", new Play("As You Like It", "comedy"));
-        plays.put("othello", new Play("Othello", "tragedy"));
+    private Play playFor(Performance performance) {
+        return plays.get(performance.playID);
+    }
 
+    private double amountFor(Performance performance) {
+        double result;
+        switch (playFor(performance).type) {
+            case "tragedy":
+                result = 40000;
+                if (performance.audience > 30) {
+                    result += 1000 * (performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (performance.audience > 20) {
+                    result += 10000 + 500 * (performance.audience - 20);
+                }
+                result += 300 * performance.audience;
+                break;
+            default:
+                throw new IllegalStateException("Unkonwn type:" + playFor(performance).type);
+        }
+        return result;
+    }
+
+    private int volumeCreditsFor(Performance performance) {
+        int volumeCredits = Math.max(performance.audience - 30, 0);
+        if ("comedy".equals(playFor(performance).type)) {
+            volumeCredits += Math.floor(performance.audience / 5);
+        }
+        return volumeCredits;
+    }
+
+    public static void main(String[] args) {
         List<Performance> performances = Lists.newArrayList();
         performances.add(new Performance("hamlet", 55));
         performances.add(new Performance("as-like", 35));
         performances.add(new Performance("othello", 40));
         Invoice invoice = new Invoice("BigCo", performances);
 
-        System.out.println(new PerformanceGroup().statement(invoice, plays));
+        System.out.println(new PerformanceGroup().statement(invoice));
     }
 }
--- src/test/java/chapter1/PerformanceGroupTest.java
+++ src/test/java/chapter1/PerformanceGroupTest.java
@@ -1,12 +1,10 @@
 package chapter1;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import java.util.List;
-import java.util.Map;
 import org.junit.Test;
 
 /**
@@ -16,11 +14,6 @@ public class PerformanceGroupTest {
 
     @Test
     public void should_align_with_base_line() {
-        Map<String, Play> plays = Maps.newConcurrentMap();
-        plays.put("hamlet", new Play("Hamlet", "tragedy"));
-        plays.put("as-like", new Play("As You Like It", "comedy"));
-        plays.put("othello", new Play("Othello", "tragedy"));
-
         List<Performance> performances = Lists.newArrayList();
         performances.add(new Performance("hamlet", 55));
         performances.add(new Performance("as-like", 35));
@@ -34,7 +27,7 @@ public class PerformanceGroupTest {
             "Amount owed is $1,730.00\n" +
             "You earned 47 credits\n";
 
-        String result = new PerformanceGroup().statement(invoice, plays);
+        String result = new PerformanceGroup().statement(invoice);
         assertThat(result, is(baseLine));
     }
 }
